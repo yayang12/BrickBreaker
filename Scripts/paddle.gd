58f9b4530200fc280931000c8daadf69bd2f8b2e
vivianw1 <122054036+vivianw1@users.noreply.github.com>
2023-10-30 13:04:39 -0700

    Checkpoint 1

--- Scripts/paddle.gd
+++ Scripts/paddle.gd
@@ -1,11 +1,36 @@
 extends RigidBody2D
 
+class_name Paddle
 
-# Called when the node enters the scene tree for the first time.
+var direction = Vector2.ZERO
+var camera_rect: Rect2
+var half_paddle_width: float
+
+@export var speed = 200
+@export var camera: Camera2D
+
+@onready var collision_shape_2d = $CollisionShape2D
+
 func _ready():
-	pass # Replace with function body.
+	camera_rect = camera.get_viewport_rect()
+	half_paddle_width = collision_shape_2d.shape.get_rect().size.x / 2 * scale.x
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	linear_velocity = speed * direction
+	
 func _process(delta):
-	pass
+	var camera_start_x = camera.position.x - camera_rect.size.x / 2
+	var camera_end_x = camera_start_x + camera_rect.size.x
+	
+	if global_position.x - half_paddle_width < camera_start_x:
+		global_position.x = camera_start_x + half_paddle_width
+	elif global_position.x + half_paddle_width > camera_end_x:
+		global_position.x = camera_end_x - half_paddle_width
+		
+func _input(event):
+	if Input.is_action_pressed("left"):
+		direction = Vector2.LEFT
+	elif Input.is_action_pressed("right"):
+		direction =  Vector2.RIGHT
+	else:
+		direction = Vector2.ZERO
