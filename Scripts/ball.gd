ed9e28342f2df1acc1b843c14ded48efa3188a9d
yayang12 <122055034+yayang12@users.noreply.github.com>
2023-11-13 13:32:57 -0800

    pc 5 done

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -6,7 +6,7 @@ signal  life_lost
 
 const VELOCITY_LIMIT = 40
 
-@export var ball_speed = 20
+@export var ball_speed = 15
 @export var lives = 3
 @export var death_zone: DeathZone
 @export var ui: UI
@@ -71,8 +71,15 @@ func ball_collision(collider):
 	var collision_x = (ball_center_x - collider_center_x) / (collider_width / 2)
 	
 	var new_velocity = Vector2.ZERO
-	s
+	
 	new_velocity.x = velocity_xy * collision_x
 	
 	if collider.get_rid() == last_collider_id && collider is Brick:
-		new_velocity.x = new_velocity.rotated(deg_to_rad(ranf_range(-45, 45))).x * 10
+		new_velocity.x = new_velocity.rotated(deg_to_rad(randf_range(-45, 45))).x * 10
+	else: 
+		last_collider_id == collider.get_rid()
+	
+	new_velocity.y = sqrt(absf(velocity_xy * velocity_xy - new_velocity.x * new_velocity.x)) * (-1 if velocity.y > 0 else 1)
+	var speed_multiplier = speed_up_factor if collider is Paddle else 1
+	
+	velocity = (new_velocity * speed_multiplier).limit_length(VELOCITY_LIMIT)
